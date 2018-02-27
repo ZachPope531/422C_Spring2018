e10152bcbaf55521f9fa039e639e10387152d076
ZachPope531 <zachary.pope531@gmail.com>
2018-02-27 15:06:09 -0600

    Changed where WordLadderTester was getting its dictionary back to original

--- Lab3/Lab3/src/Lab3/WordLadderTester.java
+++ Lab3/Lab3/src/Lab3/WordLadderTester.java
@@ -24,7 +24,7 @@ public class WordLadderTester {
 	@BeforeClass
 	public static void setUp() {
 		Main.initialize();
-		dict = Main.dict;
+		dict = Main.makeDictionary();
 		outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 	}
