b85e153489ebefefeee158dbe76f405346d2156c
ZachPope531 <zachary.pope531@gmail.com>
2018-04-04 15:07:58 -0500

    Made basic GUI

--- Lab5/src/assignment5/Main.java
+++ Lab5/src/assignment5/Main.java
@@ -1,6 +1,9 @@
 package assignment5;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("Critters.fxml"));
+        primaryStage.setTitle("Critters");
+        primaryStage.setScene(new Scene(root, 700, 420));
+        primaryStage.show();
     }
 }
